f69c6119100f5da9363230fdb0b5f6e6babfa470
Umair Jibran <umairjibran7@gmail.com>
2021-04-13 00:01:52 +0500

    set un-necassary scrolling on home

--- lib/screens/screen_home.dart
+++ lib/screens/screen_home.dart
@@ -47,69 +47,69 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height * 0.794,
-                width: MediaQuery.of(context).size.width,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.794,
+              width: MediaQuery.of(context).size.width,
+              child: SingleChildScrollView(
                 child: screens[selectedScreen],
               ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.1,
-                width: MediaQuery.of(context).size.width,
-                color: Theme.of(context).primaryColor,
-                child: Row(
-                  children: [
-                    InkWell(
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.5,
-                        child: Center(
-                          child: Text(
-                            "Home",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                            ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.1,
+              width: MediaQuery.of(context).size.width,
+              color: Theme.of(context).primaryColor,
+              child: Row(
+                children: [
+                  InkWell(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: Center(
+                        child: Text(
+                          "Home",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
-                      onTap: () {
-                        if (selectedScreen != 0)
-                          setState(() {
-                            selectedScreen = 0;
-                          });
-                      },
                     ),
-                    InkWell(
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.5,
-                        child: Center(
-                          child: Text(
-                            "Notifications",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                            ),
+                    onTap: () {
+                      if (selectedScreen != 0)
+                        setState(() {
+                          selectedScreen = 0;
+                        });
+                    },
+                  ),
+                  InkWell(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: Center(
+                        child: Text(
+                          "Notifications",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
-                      onTap: () {
-                        if (selectedScreen != 1)
-                          setState(() {
-                            selectedScreen = 1;
-                          });
-                      },
                     ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                    onTap: () {
+                      if (selectedScreen != 1)
+                        setState(() {
+                          selectedScreen = 1;
+                        });
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );

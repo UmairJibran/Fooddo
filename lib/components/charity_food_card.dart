27c548e089e5bd553755936ba9eff1e8ab0a632b
Umair Jibran <umairjibran7@gmail.com>
2021-06-27 22:47:10 +0500

    integerated seen donation request

--- lib/components/charity_food_card.dart
+++ lib/components/charity_food_card.dart
@@ -2,22 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:fooddo/classes/donation.dart';
 import 'package:fooddo/screens/screen_donation_details.dart';
 
-class CharityFoodCard extends StatelessWidget {
+import '../services.dart';
+
+class CharityFoodCard extends StatefulWidget {
   final Donation donation;
   final double height;
   CharityFoodCard({
     this.donation,
     this.height,
   });
+
+  @override
+  _CharityFoodCardState createState() => _CharityFoodCardState();
+}
+
+class _CharityFoodCardState extends State<CharityFoodCard> {
   @override
   Widget build(BuildContext context) {
+    widget.donation.seen ??= true;
     return Container(
-      height: height,
+      height: widget.height,
       margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
       padding: EdgeInsets.all(20),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: widget.donation.seen ? Colors.transparent : Colors.blue,
+        ),
         boxShadow: [
           new BoxShadow(
             blurRadius: 20,
@@ -26,11 +38,20 @@ class CharityFoodCard extends StatelessWidget {
         ],
       ),
       child: InkWell(
-        onTap: () {
+        onTap: () async {
+          if (!widget.donation.seen) {
+            await Services.donationSeen(widget.donation.id);
+            final index = Data.unclaimedDonations.indexWhere((d) {
+              return d.id == widget.donation.id;
+            });
+            setState(() {
+              Data.unclaimedDonations[index].seen = true;
+            });
+          }
           Navigator.of(context).pushNamed(
             DonationDetails.routeName,
             arguments: {
-              "donation": donation,
+              "donation": widget.donation,
             },
           );
         },
@@ -41,16 +62,16 @@ class CharityFoodCard extends StatelessWidget {
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(10),
                 child: Image.network(
-                  donation.imgUrl,
-                  height: height * 0.7,
-                  width: height * 0.7,
+                  widget.donation.imgUrl,
+                  height: widget.height * 0.7,
+                  width: widget.height * 0.7,
                   fit: BoxFit.cover,
                 ),
               ),
             ),
             SizedBox(width: 10),
             Container(
-              height: height * 0.7,
+              height: widget.height * 0.7,
               width: MediaQuery.of(context).size.width * 0.38,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -65,27 +86,27 @@ class CharityFoodCard extends StatelessWidget {
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      if (donation.status == "completed")
+                      if (widget.donation.status == "completed")
                         Icon(
                           Icons.done_all_outlined,
                           color: Colors.blue[700],
                         )
-                      else if (donation.status == "waiting")
+                      else if (widget.donation.status == "waiting")
                         Icon(
                           Icons.schedule_outlined,
                           color: Colors.indigo,
                         )
-                      else if (donation.status == "accepted")
+                      else if (widget.donation.status == "accepted")
                         Icon(
                           Icons.check,
                           color: Colors.green,
                         )
-                      else if (donation.status == "rejected")
+                      else if (widget.donation.status == "rejected")
                         Icon(
                           Icons.close,
                           color: Colors.red,
                         )
-                      else if (donation.status == "collecting")
+                      else if (widget.donation.status == "collecting")
                         Icon(
                           Icons.local_shipping_outlined,
                           color: Colors.cyan,
@@ -93,20 +114,21 @@ class CharityFoodCard extends StatelessWidget {
                     ],
                   ),
                   Text(
-                    "${donation.serving} People",
+                    "${widget.donation.serving} People",
                     style: TextStyle(
                       fontSize: 18,
                     ),
                   ),
                   Text(
-                    "at ${donation.recepient}",
+                    "at ${widget.donation.recepient}",
+                    softWrap: true,
                     style: TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                   Text(
-                    "on ${donation.date}",
+                    "on ${widget.donation.date}",
                     style: TextStyle(
                       fontSize: 14,
                     ),
